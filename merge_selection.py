80fab62ce9bf3b348a3bbf4639557229fef2ace4
dchow1992 <34880730+dchow1992@users.noreply.github.com>
2018-01-15 20:13:29 -0800

    Update merge_selection.py

--- merge_selection.py
+++ merge_selection.py
@@ -1,21 +1,22 @@
 def mergeSel():
     x = hou.selectedNodes()
     
-    if(len(x) > 0):
     
-        network_editor = hou.ui.paneTabOfType(hou.paneTabType.NetworkEditor)   
-        
-        pos = network_editor.selectPosition()
-        
-        a = network_editor.pwd().createNode("merge")
-        
-        a.setPosition(pos)
-        
+    
+    network_editor = hou.ui.paneTabOfType(hou.paneTabType.NetworkEditor)   
+
+    pos = network_editor.selectPosition()
+
+    a = network_editor.pwd().createNode("merge")
+    
+    a.setPosition(pos)
+    
+    if(len(x) > 0):       
         for node in x:
             a.setNextInput(node)
         
-        network_editor.setCurrentNode(a)
+    network_editor.setCurrentNode(a)
         
-        a.setSelected(True, clear_all_selected=1)
+    a.setSelected(True, clear_all_selected=1)
         
 mergeSel()
